0f5f5eb88bfbbc0623019ec98a18b78e358c8f8d
Kiều Trí Đăng <dang.kieu@8bitrockr.com>
2017-11-25 02:59:39 +0700

    complete UI for login host

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import { default as contract } from 'truffle-contract'
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 import Login           from './Login.js'
+import PartnerLogin    from './PartnerLogin.js'
 import Host            from './Host.js'
 import HostConfirm     from './HostConfirm.js'
 import Traveler        from './Traveler.js'
 import TravelerConfirm from './TravelerConfirm.js'
 
 import VotingJSON from '../contracts/Voting.json'
+import { withRouter } from 'react-router';
 
 const provider = new Web3.providers.HttpProvider('http://www.blockathon.asia:8545/')
 const web3 = new Web3(provider)
@@ -58,7 +60,8 @@ export default class App extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path='/'           component={Login}/>
+          <Route exact path='/'           component={withRouter(Login)}/>
+          <Route path='/partner'          component={withRouter(PartnerLogin)}/>
           <Route path='/host'             component={Host}/>
           <Route path='/host-confirm'     component={HostConfirm}/>
           <Route path='/traveler'         component={Traveler}/>
