7d5107dcfbc3e06116ffdb61c38465de5c04f1d0
Pham Quan <mikun1993@gmail.com>
2017-11-25 00:15:45 +0700

    config truffle _ add reactstrap

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ if (isDeveloping) {
   });
   
   app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
+  app.use(webpackHotMiddleware(compiler, {
+    heartbeat: 2000,
+  }));
   app.get('*', function response(req, res) {
     res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
     res.end();
