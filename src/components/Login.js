3d23f0ad7b56a372620405bdb5a86cdfb448b666
Kiều Trí Đăng <dang.kieu@8bitrockr.com>
2017-11-25 00:50:37 +0700

    complete UI for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export default class Login extends Component {
     constructor(props) {
@@ -10,8 +10,22 @@ export default class Login extends Component {
 
     render() {
         return (
-            <div>
-                Login
+            <div className='login'>
+                <div className='form'>
+                    <div className='title'><h1><span>P</span>to<span>G</span>ether</h1></div>
+                    <div className='form-group'>
+                        <label>ID</label>
+                        <input type='text'/>
+                    </div>
+                    <div className='form-group'>
+                        <label>Password</label>
+                        <input type='password'/>
+                    </div>
+                    <div className='form-group'>
+                        <label></label>
+                        <button>Submit</button>
+                    </div>
+                </div>
             </div>
         );
     }
